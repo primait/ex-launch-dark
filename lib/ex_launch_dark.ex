5453677aafad58326c600e95077312ca4f4f1863
Christian Baltazar <christian.baltazar@helloprima.com>
2025-10-06 11:28:29 +0200

    Added base file structure

---
+++ lib/ex_launch_dark.ex
@@ -0,0 +1,18 @@
+defmodule ExLaunchDark do
+  @moduledoc """
+  Documentation for `ExLaunchDark`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExLaunchDark.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
