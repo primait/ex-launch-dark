fcda69544fcb8854be68c7ebb13196bd91218215
Christian Baltazar <christian.baltazar@helloprima.com>
2025-10-06 16:37:21 +0200

    enfore config keys and rename context

--- lib/ld_config.ex
+++ lib/ld_config.ex
@@ -3,5 +3,6 @@ defmodule ExLaunchDark.LDConfig do
   A struct to hold LaunchDarkly configuration information.
   """
 
+  @enforce_keys [:sdk_key]
   defstruct sdk_key: nil, base_uri: "https://app.launchdarkly.com", options: %{}
 end
