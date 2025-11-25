cca1e29592d1f513a801235fe51a95676a47bfe5
Christian Baltazar <christian.baltazar@helloprima.com>
2025-11-25 16:48:13 +0100

    Adjust config values

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExLaunchDark.MixProject do
   def project do
     [
       app: :ex_launch_dark,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps()
