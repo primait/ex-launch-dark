c16474ab17fcea8b33b077ce01f3f53d0a781cf4
Christian Baltazar <christian.baltazar@helloprima.com>
2025-12-01 09:57:46 +0100

    chore(get_feature_flag_value): fix struct spec

--- lib/ld_adapter.ex
+++ lib/ld_adapter.ex
@@ -9,7 +9,7 @@ defmodule ExLaunchDark.LDAdapter do
   @doc """
   Retrieves the value of a single feature flag for a given context.
   """
-  @spec get_feature_flag_value(atom(), String.t(), ExLaunchDark.LDContextStruct, any()) ::
+  @spec get_feature_flag_value(atom(), String.t(), %ExLaunchDark.LDContextStruct{}, any()) ::
           {:ok, any(), atom()} | {:error, any(), atom()} | {:null, any(), atom()}
   def get_feature_flag_value(project_id, flag_key, ld_context, default_value \\ false) do
     LDContextBuilder.build_context(ld_context)
