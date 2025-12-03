1f3bf8ba0d3d12cfe663d28cbc689bc37a0022bb
Christian Baltazar <christian.baltazar@helloprima.com>
2025-12-03 12:53:40 +0100

    Added missing docs for project publication

--- mix.exs
+++ mix.exs
@@ -8,6 +8,7 @@ defmodule ExLaunchDark.MixProject do
       elixir: "~> 1.18",
       package: package(),
       start_permanent: Mix.env() == :prod,
+      docs: docs(),
       deps: deps()
     ]
   end
@@ -37,7 +38,19 @@ defmodule ExLaunchDark.MixProject do
       {:ldclient, "~> 3.0", hex: :launchdarkly_server_sdk},
       {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
       {:meck, "~> 0.9.2", only: :test},
-      {:mix_bump, "~> 0.1.0", only: [:dev, :staging]}
+      {:mix_bump, "~> 0.1.0", only: [:dev, :staging]},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
+    ]
+  end
+
+  defp docs do
+    [
+      extras: [
+        "README.md": [title: "Overview"],
+        "LICENSE.md": [title: "License"]
+      ],
+      main: "readme",
+      source_url: "https://github.com/primait/ex-launch-dark",
     ]
   end
 end
