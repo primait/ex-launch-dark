58254dcc4d74c5059c8192263398a631213f8ca2
Christian Baltazar <christian.baltazar@helloprima.com>
2026-01-27 12:27:11 +0100

    Fix dialyzer

--- lib/ld_tester.ex
+++ lib/ld_tester.ex
@@ -8,20 +8,21 @@ defmodule ExLaunchDark.LDTester do
   alias ExLaunchDark.LDContextStruct
 
   @spec test_flag_value_random_context(atom(), binary(), atom() | binary() | nil) ::
-          binary() | nil
-  def test_flag_value_random_context(project_id, flag_key, default \\ nil) do
+          {atom(), any(), any()}
+  def test_flag_value_random_context(project_id, flag_key, default \\ :null) do
     random_context = build_random_context()
     LDAdapter.get_feature_flag_value(project_id, flag_key, random_context, default)
   end
 
-  @spec test_flag_value_context_kind(atom(), binary(), binary(), map() | nil) :: binary() | nil
+  @spec test_flag_value_context_kind(atom(), binary(), binary(), map() | nil) ::
+          {atom(), any(), any()}
   def test_flag_value_context_kind(project_id, flag_key, context_kind, context_attr \\ %{}) do
     random_context = build_random_context(%{"kind" => context_kind, "attributes" => context_attr})
-    LDAdapter.get_feature_flag_value(project_id, flag_key, random_context, nil)
+    LDAdapter.get_feature_flag_value(project_id, flag_key, random_context, :null)
   end
 
   @spec test_flag_value_fixed_context(atom(), binary(), binary(), binary(), map() | nil) ::
-          binary() | nil
+          {atom(), any(), any()}
   def test_flag_value_fixed_context(
         project_id,
         flag_key,
@@ -30,15 +31,15 @@ defmodule ExLaunchDark.LDTester do
         context_attr \\ %{}
       ) do
     flag_context = %LDContextStruct{
-      kind: context_kind,
       key: context_key,
+      kind: context_kind,
       attributes: context_attr
     }
 
-    LDAdapter.get_feature_flag_value(project_id, flag_key, flag_context, nil)
+    LDAdapter.get_feature_flag_value(project_id, flag_key, flag_context, :null)
   end
 
-  @spec test_all_flags_state_random(atom(), binary()) :: binary() | nil
+  @spec test_all_flags_state_random(atom(), binary()) :: map()
   def test_all_flags_state_random(project_id, context_kind) do
     random_context = build_random_context(%{"kind" => context_kind})
     LDAdapter.get_all_flags(project_id, random_context)
